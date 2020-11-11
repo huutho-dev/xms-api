e4bf8166fa31d5eb0dc9d33295bfe43d336285a7
Katekko <joaovixgon@gmail.com>
2020-11-11 17:19:19 -0300

    Colocando if para 0

--- lib/presentation/dados/materia.dados.dart
+++ lib/presentation/dados/materia.dados.dart
@@ -11,30 +11,38 @@ abstract class MateriaDados {
         MateriaData(
           id: 1,
           descricao: 'Matemática',
-          totalVerde: totalVerde == 0 ? 1 : totalVerde * rng,
-          totalAmarelo: totalAmarelo == 0 ? 1 : totalAmarelo * rng,
-          totalVermelho: totalVermelho == 0 ? 1 : totalVermelho * rng,
+          totalVerde: totalVerde == 0 ? 1 : totalVerde * (rng == 0 ? 1 : rng),
+          totalAmarelo:
+              totalAmarelo == 0 ? 1 : totalAmarelo * (rng == 0 ? 1 : rng),
+          totalVermelho:
+              totalVermelho == 0 ? 1 : totalVermelho * (rng == 0 ? 1 : rng),
         ),
         MateriaData(
           id: 2,
           descricao: 'Português',
-          totalVerde: totalVerde == 0 ? 1 : totalVerde * rng,
-          totalAmarelo: totalAmarelo == 0 ? 1 : totalAmarelo * rng,
-          totalVermelho: totalVermelho == 0 ? 1 : totalVermelho * rng,
+          totalVerde: totalVerde == 0 ? 1 : totalVerde * (rng == 0 ? 1 : rng),
+          totalAmarelo:
+              totalAmarelo == 0 ? 1 : totalAmarelo * (rng == 0 ? 1 : rng),
+          totalVermelho:
+              totalVermelho == 0 ? 1 : totalVermelho * (rng == 0 ? 1 : rng),
         ),
         MateriaData(
           id: 3,
           descricao: 'Ciências Naturais',
-          totalVerde: totalVerde == 0 ? 1 : totalVerde * rng,
-          totalAmarelo: totalAmarelo == 0 ? 1 : totalAmarelo * rng,
-          totalVermelho: totalVermelho == 0 ? 1 : totalVermelho * rng,
+          totalVerde: totalVerde == 0 ? 1 : totalVerde * (rng == 0 ? 1 : rng),
+          totalAmarelo:
+              totalAmarelo == 0 ? 1 : totalAmarelo * (rng == 0 ? 1 : rng),
+          totalVermelho:
+              totalVermelho == 0 ? 1 : totalVermelho * (rng == 0 ? 1 : rng),
         ),
         MateriaData(
           id: 4,
           descricao: 'Ciências Humanas',
-          totalVerde: totalVerde == 0 ? 1 : totalVerde * rng,
-          totalAmarelo: totalAmarelo == 0 ? 1 : totalAmarelo * rng,
-          totalVermelho: totalVermelho == 0 ? 1 : totalVermelho * rng,
+          totalVerde: totalVerde == 0 ? 1 : totalVerde * (rng == 0 ? 1 : rng),
+          totalAmarelo:
+              totalAmarelo == 0 ? 1 : totalAmarelo * (rng == 0 ? 1 : rng),
+          totalVermelho:
+              totalVermelho == 0 ? 1 : totalVermelho * (rng == 0 ? 1 : rng),
         ),
       ];
 }
